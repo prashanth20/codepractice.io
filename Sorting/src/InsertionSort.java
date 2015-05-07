a15372ffe3d42bab33b7bdd67398b3e52e65836d
Prashant Hiremath <hiremathprashants@gmail.com>
2015-05-07 23:06:58 +0530

    Add Insertion sort using Java.

---
+++ Sorting/src/InsertionSort.java
@@ -0,0 +1,36 @@
+/**
+ * Created by PrashantH on 07-May-15.
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        InsertionSort sorter = new InsertionSort();
+        sorter.sort(array);
+    }
+
+    /*
+    Insertion sort works as follows :
+    We start with sorted array, here we assume that all the element till i-1 are already sorted.
+    Then we pick one element at a time and compare it with already sorted array i.e i -1 from right
+    to left, whenever we find the correct place for the new element, we will insert it in sorted
+    array at that position.
+    */
+    public void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int j = i - 1;
+            int key = array[i];
+            while (j >= 0 && array[j] > key) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+        printSortedArray(array);
+    }
+
+    private void printSortedArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
